7b04d9255439c31f913aee7acdf5c9832af226a2
rahaman <rahaman@mcs.anl.gov>
2021-10-09 12:05:20 -0500

    Added my_almost_eq.py

--- my_abs.py
+++ my_abs.py
@@ -7,3 +7,9 @@ def my_abs(x):
             return x
     except TypeError:
         return math.nan
+    
+def my_almost_eq(x, y):
+    if my_abs(x -y ) < 1e-16:
+        return True
+    else:
+        return False
